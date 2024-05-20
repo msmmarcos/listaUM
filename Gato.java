2637bb55ce1323b98a1e37fb7da0c0e46d6e3652
msmmarcos <168425934+msmmarcos@users.noreply.github.com>
2024-05-20 07:41:52 -0300

    Create Gato.java

---
+++ Gato.java
@@ -0,0 +1,41 @@
+public class Gato {
+	
+	private String nome;
+	private String raca;
+	private int idade;
+	
+	
+	public Gato(String novoNome, String novaRaca, int novaIdade) {
+		this.nome = novoNome;
+		this.raca = novaRaca;
+		this.idade = novaIdade;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public String getRaca() {
+		return raca;
+	}
+	
+	public int getIdade() {
+		return idade;
+	}
+	
+	public void setNome(String novoNome) {
+		this.nome = novoNome;
+	}
+	
+	public void setRaca(String novaRaca) {
+		this.raca = novaRaca;
+	}
+	
+	public void setIdade(int novaIdade) {
+		this.idade = novaIdade;
+	}
+	
+	  public String miar() {
+	    	return "miau miau";
+	  }
+}
