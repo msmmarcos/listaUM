f63a03d163d5bb7b03f9a17f830a60474064edc2
msmmarcos <168425934+msmmarcos@users.noreply.github.com>
2024-05-20 07:38:05 -0300

    Create Data.java

---
+++ Data.java
@@ -0,0 +1,39 @@
+public class Data {
+	private int dia;
+	private int mes;
+	private int ano;
+	
+	public Data(int novoDia,int novoMes,int novoAno) {
+		this.dia = novoDia;
+		this.mes = novoMes;
+		this.ano = novoAno;
+	}
+	
+	public int getDia() {
+		return dia;
+	}
+	
+	public int getMes() {
+		return mes;
+	}
+	
+	public int getAno() {
+		return ano;
+	}
+	
+	public void setDia(int novoDia) {
+		this.dia = novoDia;
+	}
+	
+	public void setMes(int novoMes) {
+		this.mes = novoMes;
+	}
+	
+	public void setAno(int novoAno) {
+		this.ano = novoAno;
+	}
+	
+	public String toString() {
+		return "Seu dia é " + this.dia + "seu mes é " + this.mes + "seu ano é " + this.ano;
+	}
+}
